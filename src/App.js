c8238f427b88a5d1d1a7b4407091e490d8e5f385
Zahid Majeed <Zahid@RELYON.COM>
2019-11-19 12:55:03 +0530

    fist commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+import PostList from './components/PostList'
+import './App.css'
+class App extends Component {
+
+  render() { 
+    return (
+
+     
+        <PostList/>
+      
+    );
+  }
+}
+ 
+export default App;
